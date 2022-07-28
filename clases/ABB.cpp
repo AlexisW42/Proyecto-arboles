2b93b835da82fb266a127c3239b52826f778b8a8
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-07-28 01:53:04 -0400

    Agregado metodo para insertar nodos en el arbol

--- clases/ABB.cpp
+++ clases/ABB.cpp
@@ -16,6 +16,47 @@ bool ABB::esVacio()
         return false;
 }
 
+void ABB::insertarNodo(ArbolNodo *nodoNuevo)
+{
+    if(raiz==NULL)
+    {
+        raiz=nodoNuevo;
+        cout << "valor insertado como raiz" << endl;
+    }
+    else
+    {
+        ArbolNodo *temp =raiz;
+        while (temp!=NULL)
+        {
+            if(nodoNuevo->valor==temp->valor)
+            {
+                cout<<"el valor ya existe, inserta otro"<<endl;
+                return;
+            }
+            else if ((nodoNuevo->valor<temp->valor) && (temp->izquierdo==NULL))
+            {
+                temp->izquierdo=nodoNuevo;
+                cout << "valor insertado en la izquierda" << endl;
+                break;
+            }
+            else if(nodoNuevo->valor<temp->valor)
+            {
+                temp=temp->izquierdo;
+            }
+            else if((nodoNuevo->valor>temp->valor)&&(temp->derecho==NULL))
+            {
+                temp->derecho=nodoNuevo;
+                cout << "valor insertado en la derecha" << endl;
+                break;
+            }
+            else
+            {
+                temp=temp->derecho;
+            }
+        } 
+    }
+}
+
 void ABB::imprimir2D(ArbolNodo *r, int espacio)
 {
     if(r==NULL)
