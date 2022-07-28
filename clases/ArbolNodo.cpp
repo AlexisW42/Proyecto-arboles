0145c1c8266aefd2f7e8f2aa7ada3a1412489f1e
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-07-28 01:18:43 -0400

    Agregados atributos y metodos a la clase ArbolNodo

--- clases/ArbolNodo.cpp
+++ clases/ArbolNodo.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 #include "ArbolNodo.h"
+
+ArbolNodo::ArbolNodo()
+{
+    valor = "";
+    izquierdo = NULL;
+    derecho = NULL;
+}
+
+ArbolNodo::ArbolNodo(int v)
+{
+    valor = v;
+    izquierdo = NULL;
+    derecho = NULL;
+}
--- clases/ArbolNodo.h
+++ clases/ArbolNodo.h
@@ -1,10 +1,16 @@
 #ifndef ArbolNodo_H
 #define ArbolNodo_H
+#include <string>
 
 class ArbolNodo
 {
 private:
 public:
+    std::string valor;
+    ArbolNodo *izquierdo;
+    ArbolNodo * derecho;
+    ArbolNodo();
+    ArbolNodo(int v);
 };
 
 #endif
