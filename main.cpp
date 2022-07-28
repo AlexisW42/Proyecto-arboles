cff2ada83170dd2018ac7c984796acdb78518c20
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-07-28 11:30:02 -0400

    Agregado main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "clases/Principal.h"
+
+using namespace std;
+
+int main()
+{
+   Principal obj;
+
+   obj.leerArchivo();
+   obj.imprimirVector();
+    
+}
